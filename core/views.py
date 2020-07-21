a4015481435b7bf22abc5839151256bf0bcb914e
Prabhanjan Desai <desaiprabhanjan05@gmail.com>
2020-07-21 16:04:23 +0530

    json to api

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Third party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+# def test_view(request):
+#     data = {
+#         'name': 'John',
+#         'age': '24'
+#     }
+#     return JsonResponse(data)
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name': 'John',
+            'age': '24'
+        }
+        return Response(data)
