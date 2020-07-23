2588d0627f10381d39473519deb550d87725205c
Prabhanjan Desai <desaiprabhanjan05@gmail.com>
2020-07-23 18:55:46 +0530

    POST API DATA

--- core/views.py
+++ core/views.py
@@ -2,21 +2,26 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # Third party imports
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import PostSerializer
+from .models import Post
 
-
-# def test_view(request):
-#     data = {
-#         'name': 'John',
-#         'age': '24'
-#     }
-#     return JsonResponse(data)
 
 class TestView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, *args, **kwargs):
-        data = {
-            'name': 'John',
-            'age': '24'
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        post = qs.first()
+        # serializer = PostSerializer(qs, many=True)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -19,6 +19,7 @@ from core.views import TestView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
+    # path('rest-auth/', include('rest_auth.urls')),
     path('admin/', admin.site.urls),
-    path('',TestView.as_view(), name='test')
+    path('', TestView.as_view(), name='test'),
 ]
